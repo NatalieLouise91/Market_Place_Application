fb4119bf91f85fc53870f4b549f614e0edac7d3e
NatalieLouise91 <natalie936@hotmail.com>
2021-08-12 12:30:19 +1000

    added comments in payments controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,14 +1,14 @@
 class PaymentsController < ApplicationController
     skip_before_action :verify_authenticity_token, only: [:webhook]
   
-    # successfully made payment for loan
+    # successfully made payment for loan. store values for individual transaction in @artefact & @ purchase
     def success
       artefact_id = params[:artefactId] 
       @artefact = Artefact.find(artefact_id)
       @purchase = LoanOrder.find_by_artefact_id(artefact_id)
     end 
   
-    # set webhook method
+    # set webhook method, store collected Stripe payment metadata in artefact_id, borrower_id, loaner_id, receipt_url variables
     def webhook
       payment_intent_id = params[:data][:object][:payment_intent]
       payment = Stripe::PaymentIntent.retrieve(payment_intent_id)
@@ -19,9 +19,9 @@ class PaymentsController < ApplicationController
     
       receipt_url = payment.charges.data[0].receipt_url
 
-      LoanOrder.create(borrower_id: borrower_id, loaner_id: loaner_id, artefact_id: artefact_id, stripe_payment_id: payment_intent_id, receipt_url: receipt_url)
+      # create a new loan order once payment is successful using metadata from Stripe payment 
 
-      print "Artefact Id #{artefact_id}"
+      LoanOrder.create(borrower_id: borrower_id, loaner_id: loaner_id, artefact_id: artefact_id, stripe_payment_id: payment_intent_id, receipt_url: receipt_url)
 
       pp payment
       
