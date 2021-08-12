12d9bc9c421e5e059f2700c345dc9c180f759570
NatalieLouise91 <natalie936@hotmail.com>
2021-08-12 11:20:59 +1000

    fixed loan order buttons to ensure only those attached to the order and see the details

--- config/routes.rb
+++ config/routes.rb
@@ -4,21 +4,17 @@ Rails.application.routes.draw do
   resources :profiles
   devise_for :users
 
-
+# ^^^ using resources to implement the basic CRUD routes for loan_orders, artefacts and profiles
+# ^^^ using devise authentication for the user entity
 
   # main page
   root 'home#page'
-  # new profile
-  get '/profiles/new', to: 'profiles#new'
-  # show profile
-  get '/profile', to: 'profile#show'
-  # edit profile
-  get 'profiles/:id/edit', to: 'profiles#edit'
-  # show individual loan order
-  
+
   # successful payments of loan orders
   get '/payments/success', to: "payments#success"
+
   # ultrahook to store metadata
   post '/payments/webhook', to: 'payments#webhook'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
